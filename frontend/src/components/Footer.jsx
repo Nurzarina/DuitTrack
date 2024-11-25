b57cbbce481df953e8200b07e49d96ac59b664e8
Nurzarina <nurzarina_os@yahoo.com>
2024-11-25 13:08:07 +0800

    Updated acknowledgement and copyright in footer and updated website screenshots to include the updated footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <Container fluid className="footer-container">
             <div className="footer-content">
                 <p style={{ paddingTop: "10px" }}>
-                    © 2024 DuitTrack. All rights reserved. Image by <a href="https://www.freepik.com/free-photo/closeup-shot-malaysian-riggit-bills_27738018.htm" target="_blank" rel="noopener noreferrer">Freepik</a>
+                    © 2024 DuitTrack. All rights reserved for the website's code and design. The background image is by <a href="https://www.freepik.com/free-photo/closeup-shot-malaysian-riggit-bills_27738018.htm" target="_blank" rel="noopener noreferrer">Freepik</a> and is used with attribution under their licensing terms.
                 </p>
             </div>
         </Container>
